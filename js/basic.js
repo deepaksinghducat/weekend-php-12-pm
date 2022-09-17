e8dc5c41da8855c6c6375d2e10d12aebaff9d85c
unknown <deepaksinghgusain>
2022-09-17 13:31:30 +0530

    upto datatypes

---
+++ js/basic.js
@@ -0,0 +1,109 @@
+// console.log("fdasfsadf");
+
+// alert 
+// console.log(alert('fdsafadsf'));
+
+// // prompt 
+// var name = prompt('Enter your name');
+
+// console.log(name);
+
+// confirm 
+// var boolean = confirm('Are you sure you want to delete this file?');
+// console.log(boolean);
+
+//  document.write
+// document.write('fsadfdasf');
+
+// variables
+
+// declaration and defination
+
+// var name = "dfassafasdfasf";
+// console.log(name);
+// var name = "fdsafasfsadf"
+
+// Let  and const
+
+// let name ="fdsafasfdafs";
+// console.log(name);
+
+// name = "fdasfasffasd"
+// console.log(name);
+
+// const 
+// const name = "Fdasfasf"
+// console.log(name);
+
+
+// hoisting
+
+// console.log(name);
+
+// var name = "hello world";
+
+// datatype 
+
+// number ,bigInt , string ,boolean, undefined , null ,array , object , typeof 
+
+
+// number
+
+// var number = parseInt(10.121212);
+// console.log(typeof(number));
+
+// bigInt
+
+// array 
+
+// let array = [false, 10, {name: "fsdaf"}]
+
+// 153 = 153 + sizeof datatype * index
+
+// 153 = 153 + 1 * 1
+
+// console.log(typeof(array));
+
+// string
+
+// const name = "hello world";
+// console.log(name);
+
+// boolean
+
+// let bool = true;
+// console.log(bool);
+
+// null 
+
+// let n = null;
+// console.log(typeof(n));
+
+// undefined
+// let a = undefined;
+
+// console.log(typeof(a));
+
+// object 
+
+// let object = {
+//     name: "hello world",
+//     fullName: function() {
+//         return this.name
+//     }
+// }
+
+// console.log(object.fullName());
+
+
+
+
+
+
+
+
+
+
+
+
+
