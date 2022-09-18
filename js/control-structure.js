2eb3635208c611e51bd47a2eb31fd9ce7b4153d3
unknown <deepaksinghgusain>
2022-09-18 13:35:06 +0530

    upto control structure

---
+++ js/control-structure.js
@@ -0,0 +1,148 @@
+// operator
+
+
+// let a = 10;
+// let b = 20;
+
+// let c = a +  b;
+
+// console.log(c);
+
+// unary
+
+// a++;
+
+// binary
+
+
+// let a = 10;
+// let b = 20;
+
+// let c = a +  b;
+
+// ternary
+
+// let a = 50;
+// let b = 100;
+
+// a === 50 ? a : b;
+
+
+// Control structure 
+// if elseif else switch case break continue 
+
+
+// if 
+
+// let age = 17;
+
+// if (age >= 18) {
+
+//     console.log("person can vote");
+
+// }else {
+//     console.log("person cannot vote");
+// }
+
+
+// else if
+
+// let number = 4;
+
+// if(number%3 === 0 && number%2 === 0) {
+//     console.log("number is divisible by 3 nnd 2");
+// }else if(number%2 === 0){
+//     console.log("number is divisible by 2");
+
+// }else{
+//     console.log("number is not divisible by 2 nor 3");
+// }
+
+// let a = 10;
+
+// let b = 20;
+
+// let key = prompt("enter the key(add, sub, mult, rem)");
+
+// switch (key) {
+//     case "add":
+//         console.log(a + b);
+//         break;
+    
+//         case "sub":
+//         console.log(a - b);
+//         break;
+
+//     default:
+//         console.log("none of the key is satisfied");
+// }
+
+// Loops
+
+// let array = [10,20,30,40,50,60,70,80,90,100];
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(i,array[i]);
+// }
+
+// array.forEach(function(value,index,arr) {
+//     console.log(value,index,arr);
+// })
+
+// for (const key in array) {
+//    console.log(key,array[key]);
+// }
+
+// for (const value of array) {
+//     console.log(value);
+// }
+
+
+// let number = 102;
+
+// while(number > 0) {
+//     console.log(number);
+//     number--;
+// }
+
+
+// do {
+
+//     console.log(number);
+//     number--;
+
+// }while(number > 100);
+
+// for foreach for in for of while do while
+
+
+// Palindrome program 
+
+// let number = 121;
+
+// let reverse = 0;
+
+// let temp = number;
+
+// while(temp > 0) {
+//     let rem = temp % 10;
+
+//     reverse = reverse * 10 + rem;
+
+//     temp = parseInt(temp/10);
+// }
+
+// console.log(number, reverse);
+
+// if(number === reverse) {
+//     console.log("number is palindrome");
+// }else{
+//     console.log("number is not palindrome");
+// }
+
+
+// write a program to check number is armstrong or not;
+
+// 153  = 1*1*1 + 5*5*5 + 3*3*3
+
+
